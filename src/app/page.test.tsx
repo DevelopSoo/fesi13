0055519d763bab6d1913859d54d97b11c0bae871
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-03-09 11:19:45 +0900

    배포

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,10 +1,6 @@
-import { render, screen } from "@testing-library/react";
-import Home from "./page";
+import { render, screen } from '@testing-library/react';
+import Home from './page';
 
-describe("MSW 모킹 테스트", () => {
-  test("테스트", () => {
-    render(<Home />);
-    const element = screen.getByText("테스트");
-    expect(element).toBeInTheDocument();
-  });
+describe('MSW 모킹 테스트', () => {
+  test('테스트', () => {});
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 // src/app/page.tsx
 
-'use client';
-
 export default function Home() {
-  const a = 1;
-  return <h1>테스트</h1>;
+  return <h1>환경: {process.env.NEXT_PUBLIC_MY_NAME}</h1>;
 }
